2efe84a8f11ea6048accc18cb2b9d3a1a70b8d3d
zxb5102 <zxb5102@163.com>
2018-06-27 17:35:06 +0800

    '添加滚轮事件'

--- js/creative.js
+++ js/creative.js
@@ -4,11 +4,11 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-(function($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top)
@@ -23,7 +23,7 @@
     })
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
     });
 
@@ -44,5 +44,30 @@
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
+
+    var whellTime;
+    $('body').mousewheel(function (event, delta, deltaX, deltaY) {
+        clearTimeout(whellTime);
+        // console.log(11);
+        whellTime = window.setTimeout(function () {
+            // console.log(11);
+            var flagEle, next;
+
+            if (delta === 1) {
+                next = $('li.active').prev();
+            } else {//向下
+                next = $('li.active').next();
+            }
+            if (next.length === 0) {
+                return;
+            }
+            flagEle = $(next.find('a').attr('href'))
+            $('html, body').stop().animate({
+                scrollTop: (flagEle.offset().top)
+            }, 1250, 'easeInOutExpo');
+        }, 100);
+        event.preventDefault();
+        // event.
+    });
 
 })(jQuery); // End of use strict
